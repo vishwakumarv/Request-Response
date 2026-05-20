62efee89987e3ac8500fa5fc9d7f43bfd3598fdb
Vishwa <vishwakumarv05@gmail.com>
2026-05-20 13:05:26 +0530

    Update TestResult.cs

--- TestResult.cs
+++ TestResult.cs
@@ -1,5 +1,13 @@
 namespace Edj20Tester.Models
 {
+    public enum ModbusFunction
+    {
+        FC01_ReadCoils = 1,
+        FC02_ReadDiscreteInputs = 2,
+        FC03_ReadHoldingRegisters = 3,
+        FC04_ReadInputRegisters = 4,
+    }
+
     public class TestResult
     {
         public string TestPoint { get; set; }
